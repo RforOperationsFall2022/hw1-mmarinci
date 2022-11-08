da4d441a59fa8c2d38d012870bcc83c6104ee721
michaela-marincic <michaela.g.marincic@gmail.com>
2022-11-07 20:57:42 -0500

    Import and Clean Data
    Data are ready to use in graphs.

--- app.R
+++ app.R
@@ -9,31 +9,39 @@
 
 library(shiny)
 library(rsconnect)
+library(tools)
 
-ob2020 <- read.csv("2020-operating.csv")
-ob2021 <- read.csv("2021-amended-operating.csv")
-ob2022 <- read.csv("2022-amended-operating")
+ob2020 <- read.csv(file = "2020-operating.csv")
+ob2021 <- read.csv(file = "2021-amended-operating.csv")
+ob2022 <- read.csv(file = "2022-amended-operating.csv")
 budget <- rbind(ob2020, ob2021, ob2022)
 
+data <- budget %>% 
+  filter(Type == "Expenditure") %>%
+  select(Year, Department, Amount) %>%
+  group_by(Year, Department) %>%
+  summarize(Total = sum(Amount)) %>%
+  arrange(Total)
+
 # Define UI for application that draws a histogram
-ui <- fluidPage(theme = shinytheme("superhero"),
+ui <- fluidPage(
 
     # Application title
-    titlePanel("Old Faithful Geyser Data"),
+    titlePanel("Operating Budget for PIttsburgh 2020-2022"),
 
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
         sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
+           # sliderInput("bins",
+            #            "Number of bins:",
+             #           min = 1,
+              #          max = 50,
+               #         value = 30)
         ),
 
         # Show a plot of the generated distribution
         mainPanel(
-           plotOutput("distPlot")
+           plotOutput("deptBarplot")
         )
     )
 )
@@ -41,13 +49,14 @@ ui <- fluidPage(theme = shinytheme("superhero"),
 # Define server logic required to draw a histogram
 server <- function(input, output) {
 
-    output$distPlot <- renderPlot({
+    output$deptBarplot <- renderPlot({
         # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
+        x <- budget[, c('Department', 'Amount')]
+        x <- aggregate(x$Amount, by=list(Category = x$Department), FUN = sum)
+        #bins <- seq(min(x), max(x), length.out = input$bins + 1)
 
         # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
+        barplot(x, col = 'darkgray', border = 'white')
     })
 }
 
