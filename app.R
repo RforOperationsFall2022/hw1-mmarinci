73924d549b48a6994a056013d679bf9f4f4567c4
michaela-marincic <michaela.g.marincic@gmail.com>
2022-11-11 19:42:36 -0500

    Add Years of Data and Line Chart
    Added budget data for years 2012-2019. Changed the dumbbell graph to a line chart of budget over time.

--- app.R
+++ app.R
@@ -16,23 +16,58 @@ library(ggalt)
 library(DT)
 library(wesanderson)
 
-ob2020 <- read.csv(file = "2020-operating.csv")
-ob2021 <- read.csv(file = "2021-amended-operating.csv")
-ob2022 <- read.csv(file = "2022-amended-operating.csv")
-budget <- rbind(ob2020, ob2021, ob2022)
 
-data <- budget %>% 
+ob2022 <- read.csv(file = "2022-amended-operating.csv")
+ob2021 <- read.csv(file = "2021-amended-operating.csv")
+ob2020 <- read.csv(file = "2020-operating.csv")
+ob2019 <- read.csv(file = "2019-operating.csv")
+ob2018 <- read.csv(file = "2018-operating.csv")
+ob2017 <- read.csv(file = "2017-operating.csv")
+ob2016 <- read.csv(file = "2016-operating.csv")
+ob2015 <- read.csv(file = "2015-operating.csv")
+ob2014 <- read.csv(file = "2014-operating.csv")
+ob2013 <- read.csv(file = "2013-operating.csv")
+ob2012 <- read.csv(file = "2012-operating.csv")
+
+budget <- rbind(ob2022, ob2021, ob2020, ob2019, ob2018, ob2017, ob2016, ob2015)
+
+data1 <- budget %>% 
   filter(Type == "Expenditure") %>%
-  select(Year, Department, Amount) %>%
+  select(Year, Department, Amount)
+
+data1$Department <- data1$Department %>%
+  replace(data1$Department == "PS - ADMIN AND SUPPORT SERVICE", "ADMIN AND SUPPORT SERVICES") %>%
+  replace(data1$Department == "PS - ADMIN AND SUPPORT SERVICES", "ADMIN AND SUPPORT SERVICES") %>%
+  replace(data1$Department == "BUREAU OF NEIGHBORHOOD EMPOWER", "BUREAU OF NEIGHBORHOOD EMPOWERMENT") %>%
+  replace(data1$Department == "OFFICE OF MANAGEMENT AND BUDGE", "OFFICE OF MANAGEMENT AND BUDGET") %>%
+  replace(data1$Department == "OFFICE OF MUNICIPAL INVESTIGAT", "OFFICE OF MUNICIPAL INVESTIGATIONS") %>%
+  replace(data1$Department == "PERMITS LICENSES AND INSPECTIO", "PERMITS LICENSES AND INSPECTIONS") %>%
+  replace(data1$Department == "PS - BBI", "BUREAU OF BUILDING INSPECTION") %>%
+  replace(data1$Department == "PS - BUREAU OF ANIMAL CARE AND", "BUREAU OF ANIMAL CARE AND CONTROL") %>%
+  replace(data1$Department == "PS - BUREAU OF ANIMAL CARE AND CONTROL", "BUREAU OF ANIMAL CARE AND CONTROL") %>%
+  replace(data1$Department == "PS - EMERGENCY MED SERVICES", "EMERGENCY MED SERVICES") %>%
+  replace(data1$Department == "PS - FIRE BUREAU", "FIRE BUREAU") %>%
+  replace(data1$Department == "PS - POLICE BUREAU", "POLICE BUREAU") %>%
+  replace(data1$Department == "PW- BUREAU OF ADMINISTRATION", "BUREAU OF ADMINISTRATION") %>%
+  replace(data1$Department == "PW- BUREAU OF PW OPERATIONS", "BUREAU OF PW OPERATIONS") %>%
+  replace(data1$Department == "PW- ENVIRONMENTAL SERVICES", "ENVIRONMENTAL SERVICES") %>%
+  replace(data1$Department == "PW - BUREAU OF ADMINISTRATION", "BUREAU OF ADMINISTRATION") %>%
+  replace(data1$Department == "PW - BUREAU OF FACILITIES", "BUREAU OF FACILITIES") %>%
+  replace(data1$Department == "PW - BUREAU OF PW OPERATIONS", "BUREAU OF PW OPERATIONS") %>%
+  replace(data1$Department == "PW - ENGINEERING & CONSTRUCTIO", "ENGINEERING AND CONSTRUCTION") %>%
+  replace(data1$Department == "PW - ENVIRONMENTAL SERVICES", "ENVIRONMENTAL SERVICES")
+
+data <- data1 %>%
   group_by(Year, Department) %>%
   summarize(Total = sum(Amount)) %>%
-  arrange(Total) %>%
+  arrange(Department) %>%
   group_by(Year) %>%
   mutate(Percentage = round(Total / sum(Total) * 100, 1))
 
+
 data_wide <- data %>%
   pivot_wider(names_from = Year, values_from = c(Total, Percentage)) %>%
-  arrange(Percentage_2020)
+  arrange(Percentage_2022)
 
 data_wide$Department <- factor(data_wide$Department, levels = as.character(data_wide$Department))
 
@@ -40,7 +75,7 @@ data_wide$Department <- factor(data_wide$Department, levels = as.character(data_
 ui <- fluidPage(
 
     # Application title
-    titlePanel("Operating Budget for Pittsburgh 2020-2022"),
+    titlePanel("Operating Budget for Pittsburgh 2012-2022"),
 
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
@@ -48,14 +83,14 @@ ui <- fluidPage(
           # Select a year of data to show ----------------------------------
           selectInput(inputId = "year", 
                       label = "Choose a budget year:",
-                      choices = c(2020, 2021, 2022), 
+                      choices = c(2022, 2021, 2020, 2019, 2018, 2017, 2016, 2015, 2014, 2013, 2012), 
                       selected = "2022"),
         
           # Select a department to show ----------------------------------
           checkboxGroupInput(inputId = "selected_dept",
                            label = "Select department(s):",
                            choices = unique(data$Department),
-                           selected = c("ETHICS BOARD","OFFICE OF EQUITY","PS - FIRE BUREAU","PS - POLICE BUREAU"))
+                           selected = c(data_wide$Department[19:29]))
           
           # Create a download button---------------------------------------
           
@@ -68,7 +103,7 @@ ui <- fluidPage(
            br(),
            br(),
            br(),
-           plotOutput("dumbbell"),
+           plotOutput("linechart"),
            br(),
            br(),
            br(),
@@ -77,7 +112,9 @@ ui <- fluidPage(
            br(),
            br(),
            DT::dataTableOutput(outputId = "budgetTable"),
-           downloadButton(outputId = "dlButton", label = "Download")
+           downloadButton(outputId = "dlButton", label = "Download Data"),
+           br(),
+           br()
         )
     )
 )
@@ -90,9 +127,9 @@ server <- function(input, output) {
     filter(data, Year %in% input$year & Department %in% input$selected_dept)
   })
   
-  dumbbell_subset <- reactive({
+  lineplot_subset <- reactive({
     req(input$selected_dept)
-    filter(data_wide, Department %in% input$selected_dept)
+    filter(data, Department %in% input$selected_dept)
   })
 
   # generate barplot--------------------------------------------------------  
@@ -105,7 +142,7 @@ server <- function(input, output) {
         theme(axis.text.x = element_text(angle = 90, size = 10)) +
         scale_y_continuous(labels = scales::comma) + 
         ylab("Total Budget ($)") +
-        theme(plot.title = element_text(hjust=0.5, face="bold"),
+        theme(plot.title = element_text(hjust=0.5, face="bold", size = 20),
             plot.background=element_rect(fill="#FFFFFF"),
             panel.background=element_rect(fill="#FFFFFF"),
             panel.grid.minor=element_blank(),
@@ -115,39 +152,35 @@ server <- function(input, output) {
         )
         })
 
-    # Generate a dumbbel chart----------------------------------------------------
-    output$dumbbell <- renderPlot({  
-      ggplot(dumbbell_subset(), aes(x=Percentage_2020, xend=Percentage_2022, y=Department, group=Department)) + 
-        geom_dumbbell(color="lightblue", 
-                      size=2,
-                      colour_xend="blue",
-                      show.legend = TRUE) + 
-        labs(x=NULL, 
-             y=NULL, 
-             title="Departments by Percent of Total Budget for 2020 vs. 2020", 
-             subtitle="Percentage Point Change: 2020 vs 2022") +
-        theme(plot.title = element_text(hjust=0.5, face="bold"),
+    # Generate a line chart----------------------------------------------------
+    output$linechart <- renderPlot({  
+      ggplot(data = lineplot_subset(), aes(x=Year, y=Total, group=Department, color=Department)) + 
+        geom_line() +
+        geom_point() +
+        scale_y_continuous(labels = scales::comma) + 
+        labs(x=NULL,
+             y=NULL,
+             title = "Budget by Department, 2012-2022") +
+        theme(plot.title = element_text(hjust=0.5, face="bold", size = 20),
               plot.background=element_rect(fill="#FFFFFF"),
               panel.background=element_rect(fill="#FFFFFF"),
-              panel.grid.minor=element_blank(),
-              panel.grid.major.y=element_blank(),
-              panel.grid.major.x=element_line(),
-              axis.ticks=element_blank(),
+              panel.grid.major.y=element_line(color = "lightgrey"),
               panel.border=element_blank()
-          )
+        )
           })
     
     # Generate a pie chart----------------------------------------------------
+    # https://r-graph-gallery.com/piechart-ggplot2.html
     output$piechart <- renderPlot({  
       ggplot(year_subset(), aes(x = '', y = Total, fill = Department)) +
-        geom_bar(width = 1, stat = "identity") +
+        geom_bar(width = 1, stat = "identity", color = "white") +
         coord_polar("y", start = 0) +
         labs(x=NULL,
              y=NULL,
-             title = "Pie Chart of Budget by Department and Year") +
+             title = "Total Budget for Selected Departments and Year") +
         scale_y_continuous(labels = scales::comma) + 
         scale_fill_manual(values = wes_palette("Darjeeling2", 30, type = "continuous")) +
-        theme(plot.title = element_text(hjust=0.5, face="bold"),
+        theme(plot.title = element_text(hjust=0.5, face="bold", size = 20),
               plot.background=element_rect(fill="#FFFFFF"),
               panel.background=element_rect(fill="#FFFFFF"),
               panel.grid.minor=element_blank(),
@@ -159,7 +192,7 @@ server <- function(input, output) {
     
     # Print data table----------------------------------------------------------
     output$budgetTable <- DT::renderDataTable(
-        DT::datatable(data = year_subset(), 
+        DT::datatable(data = lineplot_subset(), 
                       options = list(pageLength = 10), 
                       rownames = FALSE)
     )
@@ -168,10 +201,9 @@ server <- function(input, output) {
     output$dlButton <- downloadHandler(
       filename = "Budget_Data.csv",
       content = function(file) {
-        write.csv(year_subset(), file)
+        write.csv(lineplot_subset(), file)
       }
     )
-    
 }
 
 # Run the application 
